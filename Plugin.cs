ce239aa4135fa0d2741511a0d19a3ecba4ba1888
jekdev1999 <119422376+jekdev1999@users.noreply.github.com>
2023-01-16 21:55:30 -0500

    Create Plugin.cs

---
+++ Plugin.cs
@@ -0,0 +1,199 @@
+
+namespace DocumentHyperlinkToolAddin {
+    using ESRI.ArcGIS.Framework;
+    using ESRI.ArcGIS.ArcMapUI;
+    using ESRI.ArcGIS.Editor;
+    using ESRI.ArcGIS.esriSystem;
+    using System;
+    using System.Collections.Generic;
+    using ESRI.ArcGIS.Desktop.AddIns;
+    
+    
+ 
+    internal static class ThisAddIn {
+        
+        internal static string Name {
+            get {
+                return "DocumentHyperlinkToolAddin";
+            }
+        }
+        
+        internal static string AddInID {
+            get {
+                return "{3df4b7b4-5e3b-4007-9c0c-50c3cd1f1389}";
+            }
+        }
+        
+        internal static string Company {
+            get {
+                return "USACE";
+            }
+        }
+        
+        internal static string Version {
+            get {
+                return "1.0";
+            }
+        }
+        
+        internal static string Description {
+            get {
+                return "This Add-In will create a button tool that allows users to insert hyperlinks of d" +
+                    "ocuments quickly and efficiently into a selected feature.";
+            }
+        }
+        
+        internal static string Author {
+            get {
+                return "James Kennedy";
+            }
+        }
+        
+        internal static string Date {
+            get {
+                return "2018/03/28";
+            }
+        }
+        
+        internal static ESRI.ArcGIS.esriSystem.UID ToUID(this System.String id) {
+            ESRI.ArcGIS.esriSystem.UID uid = new ESRI.ArcGIS.esriSystem.UIDClass();
+            uid.Value = id;
+            return uid;
+        }
+        
+
+        internal class IDs {
+            
+
+            internal static string DocumentHyperlinkToolButton {
+                get {
+                    return "USACE_DocumentHyperlinkToolAddin_DocumentHyperlinkToolButton";
+                }
+            }
+        }
+    }
+    
+internal static class ArcMap
+{
+  private static IApplication s_app = null;
+  private static IDocumentEvents_Event s_docEvent;
+
+  public static IApplication Application
+  {
+    get
+    {
+      if (s_app == null)
+      {
+        s_app = Internal.AddInStartupObject.GetHook<IMxApplication>() as IApplication;
+        if (s_app == null)
+        {
+          IEditor editorHost = Internal.AddInStartupObject.GetHook<IEditor>();
+          if (editorHost != null)
+            s_app = editorHost.Parent;
+        }
+      }
+      return s_app;
+    }
+  }
+
+  public static IMxDocument Document
+  {
+    get
+    {
+      if (Application != null)
+        return Application.Document as IMxDocument;
+
+      return null;
+    }
+  }
+  public static IMxApplication ThisApplication
+  {
+    get { return Application as IMxApplication; }
+  }
+  public static IDockableWindowManager DockableWindowManager
+  {
+    get { return Application as IDockableWindowManager; }
+  }
+  public static IDocumentEvents_Event Events
+  {
+    get
+    {
+      s_docEvent = Document as IDocumentEvents_Event;
+      return s_docEvent;
+    }
+  }
+  public static IEditor Editor
+  {
+    get
+    {
+      UID editorUID = new UID();
+      editorUID.Value = "esriEditor.Editor";
+      return Application.FindExtensionByCLSID(editorUID) as IEditor;
+    }
+  }
+}
+
+namespace Internal
+{
+  [StartupObjectAttribute()]
+  [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+  [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+  public sealed partial class AddInStartupObject : AddInEntryPoint
+  {
+    private static AddInStartupObject _sAddInHostManager;
+    private List<object> m_addinHooks = null;
+
+    [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Never)]
+    public AddInStartupObject()
+    {
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Never)]
+    protected override bool Initialize(object hook)
+    {
+      bool createSingleton = _sAddInHostManager == null;
+      if (createSingleton)
+      {
+        _sAddInHostManager = this;
+        m_addinHooks = new List<object>();
+        m_addinHooks.Add(hook);
+      }
+      else if (!_sAddInHostManager.m_addinHooks.Contains(hook))
+        _sAddInHostManager.m_addinHooks.Add(hook);
+
+      return createSingleton;
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Never)]
+    protected override void Shutdown()
+    {
+      _sAddInHostManager = null;
+      m_addinHooks = null;
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Never)]
+    internal static T GetHook<T>() where T : class
+    {
+      if (_sAddInHostManager != null)
+      {
+        foreach (object o in _sAddInHostManager.m_addinHooks)
+        {
+          if (o is T)
+            return o as T;
+        }
+      }
+
+      return null;
+    }
+
+    // Expose this instance of Add-in class externally
+    public static AddInStartupObject GetThis()
+    {
+      return _sAddInHostManager;
+    }
+  }
+}
+}
